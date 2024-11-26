29e53e76cfc26b6c3496bd072ff5ef1f17d3d011
Ankit Sharma <sankit72@gmail.com>
2024-11-26 11:19:21 +0530

    update article view

--- src/routes/articles/[id]/+page.svelte
+++ src/routes/articles/[id]/+page.svelte
@@ -2,6 +2,7 @@
 	import { page } from '$app/stores';
 	import { pb } from '$lib';
 	import type { Article } from '$lib/types/article';
+	import dayjs from 'dayjs';
 	import { onMount } from 'svelte';
 	let article: Article | null = $state(null);
 
@@ -16,8 +17,16 @@
 
 <main>
 	{#if article}
-		<h1>{article.title}</h1>
-		<p>{article.content}</p>
+		<div class="flex w-full items-center justify-center">
+			<div class="flex flex-col p-4 md:w-2/3 lg:w-1/2">
+				<p class="mb-2 text-slate-800">{dayjs(article.created).format('DD MMMM YYYY')}</p>
+				<h1 class="mb-4 text-3xl font-bold">{article.title}</h1>
+				<span class="mb-2 text-slate-800">
+					by <span>{article.url}</span>
+				</span>
+				<div class="prose justify-center prose-img:mx-auto">{@html article.content}</div>
+			</div>
+		</div>
 	{:else}
 		<p>No article found</p>
 	{/if}
