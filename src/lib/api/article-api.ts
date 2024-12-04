712329d8e2c6358c9967390b1ab489ff97715650
Ankit Sharma <sankit72@gmail.com>
2024-12-05 01:09:06 +0530

    remove excrept

--- src/lib/api/article-api.ts
+++ src/lib/api/article-api.ts
@@ -4,12 +4,9 @@ import { ArticleView, type Article } from '$lib/types/article';
 export async function loadArticle({ page = 0, limit = 20, filter = '' } = {}) {
 	const resultList = await pb.collection('articles').getList<ArticleView>(page, limit, {
 		sort: '-created',
-		filter: pb.filter(
-			'title ~ {:filter} || description ~ {:filter} || siteName ~ {:filter} || excerpt ~ {:filter}',
-			{
-				filter
-			}
-		)
+		filter: pb.filter('title ~ {:filter} || description ~ {:filter} || siteName ~ {:filter}', {
+			filter
+		})
 	});
 
 	resultList.items = resultList.items.map((article) => new ArticleView(article));
